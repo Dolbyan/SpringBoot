b9ebbb1cd8da387e1f6bf401df4976f27f026b03
Dolbyan <pepik767@wp.pl>
2025-05-09 18:25:31 +0200

    Corrections

--- src/test/python/test.py
+++ src/test/python/test.py
@@ -5,7 +5,7 @@ def test_server():
     time.sleep(15)
     try:
         response = requests.get("http://localhost:8080")
-        assert response.status_code == 400
+        assert response.status_code == 404
     except requests.exceptions.ConnectionError:
         assert False, "Failed connection to server"
 
