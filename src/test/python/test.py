05d42d5eafdd8dccee309c5e4f19d2f062c5a0f5
Dolbyan <pepik767@wp.pl>
2025-05-09 18:06:53 +0200

    Added workflow & dockerfile

---
+++ src/test/python/test.py
@@ -0,0 +1,11 @@
+import requests
+import time
+
+def test_server():
+    time.sleep(15)
+    try:
+        response = requests.get("http://localhost:8080")
+        assert response.status_code == 400
+    except requests.exceptions.ConnectionError:
+        assert False, "Failed connection to server"
+
